41c9734cd969ac5af6b9b54c202b6ab431df5752
ArielBac <arielvieira65@gmail.com>
2024-11-24 09:07:08 -0400

    Fixing test and make it pass for the right reason

--- MarsRover/MarsRoverShould.cs
+++ MarsRover/MarsRoverShould.cs
@@ -1,12 +1,16 @@
+using FluentAssertions;
+
 namespace MarsRover
 {
     public class MarsRoverShould
     {
         [Theory]
         [InlineData("N,0,0", "f", "N,0,1")]
-        public void ReturnNewPsotion(string startPosition, string command, string expectedNewPosition)
+        public void ReturnNewPosition(string startPosition, string command, string expectedNewPosition)
         {
             var result = MasterRoverService.Move(startPosition, command);
+
+            result.Should().Be(expectedNewPosition);
         }
     }
 }
