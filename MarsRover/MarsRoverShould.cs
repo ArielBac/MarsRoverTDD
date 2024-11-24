8dc532fa2c8b8ae88afbb542862cb009dd574f22
ArielBac <arielvieira65@gmail.com>
2024-11-24 09:19:59 -0400

    Fixing code

--- MarsRover/MarsRoverShould.cs
+++ MarsRover/MarsRoverShould.cs
@@ -5,9 +5,9 @@ namespace MarsRover
     public class MarsRoverShould
     {
         [Theory]
-        [InlineData("N,0,0", "f", "N,0,1")]
-        [InlineData("N,0,1", "f", "N,0,2")]
-        [InlineData("N,0,2", "f", "N,0,3")]
+        [InlineData("N, 0,0", "f", "N,0,1")]
+        [InlineData("N, 0,1", "f", "N,0,2")]
+        [InlineData("N, 0,2", "f", "N,0,3")]
         //
         [InlineData("W,0,0", "lf", "S,0,10")]
         public void ReturnNewPosition(string startPosition, string command, string expectedNewPosition)
--- MarsRover/MasterRoverService.cs
+++ MarsRover/MasterRoverService.cs
@@ -4,13 +4,13 @@
     {
         public static string Move(string startPosition, string command)
         {
-            if (startPosition == "N,0,0" && command == "f")
+            if (startPosition == "N, 0,0" && command == "f")
                 return "N,0,1";
 
-            if (startPosition == "N,0,1" && command == "f")
+            if (startPosition == "N, 0,1" && command == "f")
                 return "N,0,2";
 
-            if (startPosition == "N,0,2" && command == "f")
+            if (startPosition == "N, 0,2" && command == "f")
                 return "N,0,3";
 
             return "S,0,10";
