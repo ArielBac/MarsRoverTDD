1c42407b710df0abc96cd8995bbe555fa1d9e9e3
ArielBac <arielvieira65@gmail.com>
2024-11-24 10:06:10 -0400

    Refactring tests to split edge cases

--- MarsRover/MarsRoverShould.cs
+++ MarsRover/MarsRoverShould.cs
@@ -8,9 +8,16 @@ namespace MarsRover
         [InlineData("N, 0,0", "f", "N,0,1")]
         [InlineData("N, 0,1", "f", "N,0,2")]
         [InlineData("N, 0,2", "f", "N,0,3")]
-        //
-        [InlineData("W, 0,0", "lf", "S,0,10")]
+
         public void ReturnNewPosition(string startPosition, string command, string expectedNewPosition)
+        {
+            var result = MasterRoverService.Move(startPosition, command);
+
+            result.Should().Be(expectedNewPosition);
+        }
+
+        [InlineData("W, 0,0", "lf", "S,0,10")]
+        public void ReturnNewPositionWhenEdge(string startPosition, string command, string expectedNewPosition)
         {
             var result = MasterRoverService.Move(startPosition, command);
 
