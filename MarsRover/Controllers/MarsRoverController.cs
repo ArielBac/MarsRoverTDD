c7ded25b2f78efc3a6bf08428592918e357ef75e
ArielBac <arielvieira65@gmail.com>
2025-01-03 11:19:59 -0400

    Adding simple exception handling

--- MarsRover/Controllers/MarsRoverController.cs
+++ MarsRover/Controllers/MarsRoverController.cs
@@ -14,9 +14,16 @@ namespace MarsRover.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult Move(MarsRoverMoveRequest request)
         {
-            var result = MarsRoverService.Move(request.StartPosition, request.Command);
+            try
+            {
+                var result = MarsRoverService.Move(request.StartPosition, request.Command);
 
-            return Ok(new MarsRoverMoveResponse(result));
+                return Ok(new MarsRoverMoveResponse(result));
+            }
+            catch
+            {
+                return BadRequest("Process error. Please verify the input format and try again.");
+            }
         }
     }
 }
